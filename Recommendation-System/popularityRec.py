08d8fa82dc31d140eb7d3b819fa8b886d2d6f06b
Avery Brinkman <averybrinkman@gmail.com>
2021-07-05 20:53:44 -0400

    Popularity Recommendation

---
+++ Recommendation-System/popularityRec.py
@@ -0,0 +1,24 @@
+import os
+import pandas
+import numpy
+
+os.chdir('Recommendation-System')
+
+# Restaurant ratings [UserID | RestaurantID | Overall | Food | Service] (Scale 0-2)
+frame = pandas.read_csv('data/rating_final.csv')
+# IDs each restaurant [RestaurantID | TypeOfFood] 
+cuisine = pandas.read_csv('data/chefmozcuisine.csv')
+print(frame.head(),'\n')
+
+# Creates a new DataFrame by grouping data by placeID and taking the count of rating values
+rating_count = pandas.DataFrame(frame.groupby('placeID')['rating'].count())
+# Sorts data
+sorted_rating_count = rating_count.sort_values('rating', ascending=False)
+print(sorted_rating_count.head(),'\n')
+
+most_rated_places = pandas.DataFrame([135085,132825,135032,135052,132834], index=numpy.arange(5), columns=['placeID'])
+
+summary = pandas.merge(most_rated_places, cuisine, on='placeID') 
+print(summary,'\n')
+
+print(cuisine['Rcuisine'].describe(),'\n')
